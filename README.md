51506ed2965c163fbd93f03859e2c02b1f29f760
Mladen_K <kobiljski.mladen@me.com>
2017-11-26 18:47:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftLogger
+
+A description of this package.
---
+++ Sources/SwiftLogger/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  Swift Logger Server Application
+//
+//  Created by Mladen Kobiljski · https://www.linkedin.com/in/mladen-kobiljski/
+//  Copyright © 2017 Craftwell · http://www.craftwell.io
+//  MIT License · http://choosealicense.com/licenses/mit/
+//
+
+
+// import declarations
+import Foundation
+import Kitura
+import KituraNet
+
+
+
+// create a new router
+let router = Router()
+
+
+// SwiftLogger Backend Route (define handler)
+router.post("/logger", handler: loggerHandler)
+
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8080, with: router)
+
+
+// start the Kitura runloop (this call never returns)
+Kitura.run()
+
